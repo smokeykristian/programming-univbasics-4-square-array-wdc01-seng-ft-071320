b44bd17e7e29c63aec0a52200c6e2d5e6312c6ef
Kristian Portillo <portillk@gmail.com>
2020-07-06 19:36:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,6 +4,9 @@ def square_array(numbers)
 new_array = []
 numbers.length.times do |index|
   new_array.push(numbers[index] ** 2)
+numbers = [9,10,16,25]
+numbers.length.times do |index|
+  new_array.push(numbers[index] ** 2)
 end
 new_array
 end
