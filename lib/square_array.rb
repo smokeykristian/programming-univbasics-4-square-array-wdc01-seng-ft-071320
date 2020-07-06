e49b5966eba3103555dd6fbfbc8577dbce6b9403
Kristian Portillo <portillk@gmail.com>
2020-07-06 19:39:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
-def square_array(numbers)
+def square_array(array)
  numbers = [1,2,3]
 
 new_array = []
-square_array.length.times do |index|
-  new_array.push(square_array[index] ** 2)
+array.length.times do |index|
+  new_array.push(array[index] ** 2)
 end
 new_array
 end
